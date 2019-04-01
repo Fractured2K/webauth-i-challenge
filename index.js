a5d173d259b1f3fd2c4d1e5ec3a00b043f920ed8
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-01 11:48:47 -0700

    Implement middleware

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const middleware = require("./middleware/config");
 
 const server = express();
+
+// middleware
+middleware(server);
 
 server.listen(5000, () =>
 	console.log("Server is now listening on http://localhost:5000")
