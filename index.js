03bb62e0a9e554bde4d7fb8359af5a6287ee64ac
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-01 11:53:10 -0700

    Implement user route handler

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const middleware = require("./middleware/config");
 
+// controllers
+const userController = require("./controllers/users");
+
 const server = express();
 
 // middleware
 middleware(server);
+
+// route handlers
+server.use("/api/users", userController);
 
 server.listen(5000, () =>
 	console.log("Server is now listening on http://localhost:5000")
