6f9b63fc4c1f2950ea6115f9e616fd7cd3f8e025
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-01 11:39:59 -0700

    Create users add method

--- models/users.js
+++ models/users.js
@@ -1,6 +1,13 @@
 module.exports = {
+	add,
 	findById
 };
+
+async function add(user) {
+	const [id] = await db("users").insert(user);
+
+	return findById(id);
+}
 
 function findById(id) {
 	return db("users")
