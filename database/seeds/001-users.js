94957735ccb9c271e7cb16779d861327d27c8fa2
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-01 12:54:03 -0700

    Create users seed

---
+++ database/seeds/001-users.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
+
+const createFakeUser = () => ({
+	username: faker.internet.userName(),
+	password: faker.internet.password()
+});
+
+exports.seed = async function(knex, Promise) {
+	const fakeUsers = [];
+
+	for (let i = 0; i < 100; i++) {
+		fakeUsers.push(createFakeUser());
+	}
+
+	fs.writeFileSync(
+		"./authTestInfo.json",
+		JSON.stringify({ users: fakeUsers })
+	);
+
+	fakeUsers.map(user => {
+		user.password = bcrypt.hashSync(user.password, 4);
+	});
+
+	return await knex("users").insert(fakeUsers);
+};
