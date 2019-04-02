bb61455afa44d4c7701d12aa46fa833402fe04a7
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-02 11:46:30 -0700

    Create restricted middleware

---
+++ middleware/restricted.js
@@ -0,0 +1,10 @@
+/*
+    Check for user session
+*/
+module.exports = (req, res, next) => {
+	if (req.session && req.session.user) {
+		return next();
+	}
+
+	return res.status(401).json({ message: "Please login" });
+};
