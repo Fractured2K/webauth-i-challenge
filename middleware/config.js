850f6c3ee543798270d37c93670303e501dde458
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-04-02 11:34:00 -0700

    Implement session middleware

--- middleware/config.js
+++ middleware/config.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+const session = require("express-session");
+
+const sessionConfig = require("./config/sessions");
 
 module.exports = server => {
 	server.use(express.json());
@@ -10,4 +13,5 @@ module.exports = server => {
 	server.use(helmet());
 	server.use(morgan("dev"));
 	server.use(cors());
+	server.use(session(sessionConfig));
 };
